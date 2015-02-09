d89e0e366f50579baa34ef4a5c45aa76245f894a
Libo Cannici <lcannici@zendesk.com>
2015-02-09 13:58:52 +0100

    With comment

--- curly_bars/lexer.rb
+++ curly_bars/lexer.rb
@@ -4,6 +4,11 @@ module CurlyBars
   class Lexer < RLTK::Lexer
     match_first
 
+    # Comment
+    rule(/{{!/) { push_state :comment }
+    rule(/[^}]+/, :comment)
+    rule(/}}/, :comment) { pop_state }
+
     rule(/{{/) { push_state :curly; :CURLY_TAG_BEGIN }
     rule(/}}/, :curly) { pop_state; :CURLY_TAG_END }
     rule(/[^}]*/, :curly) { :IDENT }
